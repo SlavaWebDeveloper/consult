5021e7dda875ad871e0cc8284fde5270ac06ffbd
Slava Sidorov <sidorov-vv3@mail.ru>
2024-10-19 17:51:50 +0700

    refactoring code and add media code for main page

--- src/js/main.js
+++ src/js/main.js
@@ -14,3 +14,63 @@ import 'bootstrap/js/dist/scrollspy';
 import 'bootstrap/js/dist/tab';
 import 'bootstrap/js/dist/toast';
 import 'bootstrap/js/dist/tooltip';
+
+document.addEventListener('DOMContentLoaded', function () {
+  function handleScroll() {
+    const header = document.getElementById('header-all');
+    const headerNavbar = header.querySelector('.navbar');
+
+    if (window.scrollY > 50) {
+      headerNavbar.style.opacity = '0'; 
+      headerNavbar.style.pointerEvents = 'none';
+    } else {
+      headerNavbar.style.opacity = '1'; 
+      headerNavbar.style.pointerEvents = 'auto'; 
+    }
+  }
+
+  function throttle(func, limit) {
+    let lastFunc;
+    let lastRan;
+    return function() {
+      const context = this;
+      const args = arguments;
+      if (!lastRan) {
+        func.apply(context, args);
+        lastRan = Date.now();
+      } else {
+        clearTimeout(lastFunc);
+        lastFunc = setTimeout(function() {
+          if ((Date.now() - lastRan) >= limit) {
+            func.apply(context, args);
+            lastRan = Date.now();
+          }
+        }, limit - (Date.now() - lastRan));
+      }
+    };
+  }
+
+  window.addEventListener('scroll', throttle(handleScroll, 1000));
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  function adjustDropdowns() {
+    const dropdowns = document.querySelectorAll('.dropdown');
+
+    dropdowns.forEach(function(dropdown) {
+      // Убираем класс dropend при каждом вызове, чтобы не было дублирования
+      dropdown.classList.remove('dropend');
+
+      // Если ширина экрана от 320 до 900px, добавляем класс dropend
+      if (window.innerWidth >= 640 && window.innerWidth < 900) {
+        dropdown.classList.add('dropend');
+      }
+    });
+  }
+
+  // Запускаем функцию при загрузке страницы
+  adjustDropdowns();
+
+  // И при изменении размера экрана
+  window.addEventListener('resize', adjustDropdowns);
+});
